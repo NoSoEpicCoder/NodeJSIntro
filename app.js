acb63c11368e78aa9f8a839a31727660f35f096c
kevin.gilbert <NoSoEpicCoder>
2020-06-23 14:47:59 -0400

    Added a few useful modules

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 const logger =require('./logger');
 
+//Path objects
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+
+//Computer info
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
+
+console.log(`Total memory ${totalMem} and free memory ${freeMem}`);
+
+
+//File system info
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('./', function(err, files) {
+    if(err) console.log(`Error: ${error}`);
+    else console.log(`Result: ${files}`);
+});
+
+
+//Start function
 function sayHello(name) {
     console.log('Hello ' + name);
 }
 
 sayHello('Yoda');
 
+
+//Import module
 logger.log('a message');
 
 //Module is global
